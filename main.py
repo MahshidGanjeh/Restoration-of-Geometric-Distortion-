c30ab51eda1b5b24514578c0dc8cc541636828c0
Hp <mahshidga@gmail.com>
2019-07-24 22:48:42 +0430

    getting started with image + core operation

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
 import numpy as np
 import cv2
-img = cv2.imread('0021396012600015.jpg',0)
-cv2.imshow('image',img)
+
+from matplotlib import pyplot as plt
+
+image = cv2.imread('sample.jpg')
+# cv2.imshow('image',image)
+# key = cv2.waitKey()
+# if key == 27:
+#   cv2.destroyAllWindows()
+
+img = cv2.line(image, (0, 0), (314, 101), (0, 255, 0), 4)
+cv2.imshow('img', img)
 cv2.waitKey(0)
-cv2.destroyAllWindows()
+# value e pixel radif 100 va sotune 100 ra neshan midahad
+px = image[100, 100]
+#print(px)
+# B=0G=1R=2
+blue = image[100, 100, 0]
+#print(blue)
+image[100, 100] = [255, 255, 255]
+
+#print(image.item(100, 100, 0))
+
+# modifying RED value
+image.itemset((10, 10, 2), 100)
+print(image.item(10, 10, 2))
+
+# Accessing Image Properties
+# It returns a tuple of number of rows, columns and channels (if image is color):
+print(image.shape)
+print(image.size)
